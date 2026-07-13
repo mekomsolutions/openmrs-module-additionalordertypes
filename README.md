9d08ab7632d011bcc31bcdb15c14440bd98919fa
Siddharth Vaish <sidvaish97@gmail.com>
2026-07-13 17:17:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# OpenMRS Additional Order Types Module
+
+This minimal OpenMRS module starts without adding UI or database changes and writes the following INFO message when OpenMRS starts it:
+
+```text
+Additional Order Types module loaded successfully
+```
---
+++ omod/src/main/java/org/openmrs/module/additionalordertypes/AdditionalOrderTypesActivator.java
@@ -0,0 +1,18 @@
+package org.openmrs.module.additionalordertypes;
+
+import org.openmrs.module.BaseModuleActivator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * OpenMRS lifecycle callbacks for the Additional Order Types module.
+ */
+public class AdditionalOrderTypesActivator extends BaseModuleActivator {
+	
+	private static final Logger log = LoggerFactory.getLogger(AdditionalOrderTypesActivator.class);
+	
+	@Override
+	public void started() {
+		log.info("Additional Order Types module loaded successfully");
+	}
+}
